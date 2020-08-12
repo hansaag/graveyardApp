69224df24537ddccd5627abc7d1d2b1ac56fd7c8
Hans Marius Gunleiksrud <hansag91@gmail.com>
2020-08-13 00:31:06 +0200

    fixed widescreen formatting in activity list, popup boxes remain

--- client/src/components/Activities.js
+++ client/src/components/Activities.js
@@ -1,9 +1,9 @@
 import React, {
-  useState,
-  useContext,
-  Fragment,
-  useCallback,
-  useEffect,
+    useState,
+    useContext,
+    Fragment,
+    useCallback,
+    useEffect,
 } from "react";
 import { GlobalContext } from "../contexts/GlobalContext";
 import { ProjectContext } from "../contexts/ProjectContext";
@@ -12,51 +12,51 @@ import JsonActivities, { cleanProjects } from "../utilities/JsonActivities";
 import ProjectInfo from "./ProjectInfo";
 
 const Activities = () => {
-  const { value, setValue } = useContext(GlobalContext);
-  const [activities, setActivities] = useState([]);
-  const {viewProject, setViewProject} = useContext(ProjectContext);
+    const { value, setValue } = useContext(GlobalContext);
+    const [activities, setActivities] = useState([]);
+    const { viewProject, setViewProject } = useContext(ProjectContext);
 
-  let projectContainer;
+    let projectContainer;
 
-  let currentProject;
-  const getProjectData = useCallback((projArr) => {
-    projectContainer = projArr;
-    console.log(projectContainer);
-    return projArr;
-  });
+    let currentProject;
+    const getProjectData = useCallback((projArr) => {
+        projectContainer = projArr;
+        console.log(projectContainer);
+        return projArr;
+    });
 
-  const toggleProject = (activity) => {
-    console.log(activity);
-    if (activity != viewProject) {
-      setViewProject(activity);
-    } else {
-      setViewProject(null);
-    }
-  };
+    const toggleProject = (activity) => {
+        console.log(activity);
+        if (activity != viewProject) {
+            setViewProject(activity);
+        } else {
+            setViewProject(null);
+        }
+    };
 
-  useEffect(() => {
-    fetch(`http://138.68.88.7:5000/projects/${value.gy.id}`)
-      .then((response) => response.json())
-      .then((sendData) => getProjectData(sendData))
-      .then((cleaned) => setActivities(cleaned));
-  }, [value.gy.id]);
+    useEffect(() => {
+        fetch(`http://138.68.88.7:5000/projects/${value.gy.id}`)
+            .then((response) => response.json())
+            .then((sendData) => getProjectData(sendData))
+            .then((cleaned) => setActivities(cleaned));
+    }, [value.gy.id]);
 
-  const fields = activities.map((item, index) => (
-    <li
-      className="activity-list-item"
-      onClick={() => toggleProject(activities[index])}
-      key={index}
-    >
-      <a className="activity">{item.project_title}</a>
-    </li>
-  ));
+    const fields = activities.map((item, index) => (
+        <li
+            className="activity-list-item"
+            onClick={() => toggleProject(activities[index])}
+            key={index}
+        >
+            <a className="activity">{item.project_title}</a>
+        </li>
+    ));
 
-  return (
-      <div classname="activity-holder">
-        <ul className="activity-list">{fields}</ul>
-        <ProjectInfo/>
-      </div>
-  );
+    return (
+        <div classname="activity-holder">
+            <ul className="activity-list">{fields}</ul>
+            <ProjectInfo />
+        </div>
+    );
 };
 
 export default Activities;
--- client/src/components/TaskList.js
+++ client/src/components/TaskList.js
@@ -1,9 +1,9 @@
 import React, {
-  useState,
-  useContext,
-  Fragment,
-  useCallback,
-  useEffect,
+    useState,
+    useContext,
+    Fragment,
+    useCallback,
+    useEffect,
 } from "react";
 import WorkWindow from "./WorkWindow";
 import AddProject from "./AddProject";
@@ -14,123 +14,142 @@ import { FieldButtons } from "../contexts/FieldButtons";
 import { PriorityButtonContext } from "../contexts/PriorityButtonContext";
 
 import {
-  graveyards,
-  fieldActivities,
-  globalActivities,
-  extraActivities,
+    graveyards,
+    fieldActivities,
+    globalActivities,
+    extraActivities,
 } from "../utilities/GraveyardInfo";
 
 const TaskList = () => {
-  const { value, setValue } = useContext(GlobalContext);
-  const { edit, setEdit } = useContext(GlobalEdit);
-  const { allClicked, setAllClicked } = useContext(FieldButtons);
+    const { value, setValue } = useContext(GlobalContext);
+    const { edit, setEdit } = useContext(GlobalEdit);
+    const { allClicked, setAllClicked } = useContext(FieldButtons);
 
-  const [priorityNum, setPriorityNum] = useState(null);
+    const [priorityNum, setPriorityNum] = useState(null);
 
-  const [projectWindow, setProjectWindow] = useState(false);
-  const [dialog, setDialog] = useState(false);
-  const [selectedActivity, setSelectedActivity] = useState(null);
+    const [projectWindow, setProjectWindow] = useState(false);
+    const [dialog, setDialog] = useState(false);
+    const [selectedActivity, setSelectedActivity] = useState(null);
 
-  const toggleEdit = () => {
-    setEdit(!edit);
-  };
+    const toggleEdit = () => {
+        setEdit(!edit);
+    };
 
-  const exitDialog = () => {
-    if (dialog) {
-      setDialog(!dialog);
-      setAllClicked(1);
-    }
-  };
+    const exitDialog = () => {
+        if (dialog) {
+            setDialog(!dialog);
+            setAllClicked(1);
+        }
+    };
 
-  const setActivity = (activity) => {
-    setSelectedActivity(activity);
-  };
+    const setActivity = (activity) => {
+        setSelectedActivity(activity);
+    };
 
-  const openActivity = useCallback((activity) => {
-    setActivity(activity);
-    console.log("dialog clicked");
-    setDialog(!dialog);
-  });
+    const openActivity = useCallback((activity) => {
+        setActivity(activity);
+        console.log("dialog clicked");
+        setDialog(!dialog);
+    });
 
-  const openProjects = useCallback(() => {
-    setProjectWindow(!projectWindow);
-    setPriorityNum(null);
-    if (dialog) {
-      setDialog(!dialog);
-      setAllClicked(1);
-    }
+    const openProjects = useCallback(() => {
+        setProjectWindow(!projectWindow);
+        setPriorityNum(null);
+        if (dialog) {
+            setDialog(!dialog);
+            setAllClicked(1);
+        }
 
-    console.log("project clicked");
-  });
+        console.log("project clicked");
+    });
 
-  const weeklyActivities = fieldActivities.map((activity, index) => (
-    <li className="input-activity-listitem">
-      <div onClick={() => openActivity(activity)}>
-        <img className="input-img" src={activity.img}></img>
-        <p>{activity.value}</p>
-      </div>
-    </li>
-  ));
+    const weeklyActivities = fieldActivities.map((activity, index) => (
+        <li className="input-activity-listitem">
+            <div onClick={() => openActivity(activity)}>
+                <div className="input-img-holder">
+                    <img className="input-img" src={activity.img}></img>
+                </div>
+                <div className="input-text-holder">
+                    <p>{activity.value}</p>
+                </div>
+            </div>
+        </li>
+    ));
 
-  const generalActivities = globalActivities.map((activity) => (
-    <li
-      className="input-activity-listitem"
-      onClick={() => openActivity(activity)}
-    >
-      <div>
-        <img className="input-img" src={activity.img}></img>
-        <p>{activity.value}</p>
-      </div>
-    </li>
-  ));
+    const generalActivities = globalActivities.map((activity) => (
+        <li
+            className="input-activity-listitem"
+            onClick={() => openActivity(activity)}
+        >
+            <div>
+                <div className="input-img-holder">
+                    <img className="input-img" src={activity.img}></img>
+                </div>
 
-  const extras = extraActivities.map((activity) => (
-    <li
-      className="input-activity-listitem"
-      onClick={() => openActivity(activity)}
-    >
-      <div>
-        <img className="input-img" src={activity.img}></img>
-        <p>{activity.value}</p>
-      </div>
-    </li>
-  ));
+                <div className="input-text-holder">
+                    <p>{activity.value}</p>
+                </div>
+            </div>
+        </li>
+    ));
 
-  useEffect(() => {}, [dialog]);
+    const extras = extraActivities.map((activity) => (
+        <li
+            className="input-activity-listitem"
+            onClick={() => openActivity(activity)}
+        >
+            <div>
+                <div className="input-img-holder">
+                    <img className="input-img" src={activity.img}></img>
+                </div>
+                <div className="input-text-holder">
+                    <p>{activity.value}</p>
+                </div>
+            </div>
+        </li>
+    ));
 
-  return (
-    <div
-      className={dialog ? "input-container-dimmed" : "input-container"}
-      // onClick={() => exitDialog()}
-    >
-      <div className="input-button-container">
-        <div className="extra-activities-button" onClick={openProjects}>
-          Legg til prosjekt
+    useEffect(() => {}, [dialog]);
+
+    return (
+        <div
+            className={dialog ? "input-container-dimmed" : "input-container"}
+            // onClick={() => exitDialog()}
+        >
+            <div className="input-button-container">
+                <div className="extra-activities-button" onClick={openProjects}>
+                    Legg til prosjekt
+                </div>
+                <div className="back-button" onClick={() => toggleEdit()}>
+                    Tilbake
+                </div>
+            </div>
+            <PriorityButtonContext.Provider
+                value={{ priorityNum, setPriorityNum }}
+            >
+                <AddProject exit={openProjects} project={projectWindow} />
+            </PriorityButtonContext.Provider>
+
+            <WorkWindow
+                exit={exitDialog}
+                dia={dialog}
+                activity={selectedActivity}
+            />
+            <div className="task-list-title">
+                <p>
+                    Velg utført aktivitet <span>({value.gy.value})</span>
+                </p>
+            </div>
+
+            <div className="input-list-container">
+                <ul className="input-activity-list">
+                    {weeklyActivities}
+                    {generalActivities}
+                    {extras}
+                </ul>
+            </div>
         </div>
-        <div className="back-button" onClick={() => toggleEdit()}>
-          Tilbake
-        </div>
-      </div>
-      <PriorityButtonContext.Provider value={{ priorityNum, setPriorityNum }}>
-        <AddProject exit={openProjects} project={projectWindow} />
-      </PriorityButtonContext.Provider>
-
-      <WorkWindow exit={exitDialog} dia={dialog} activity={selectedActivity} />
-      <div className="task-list-title">
-        <p>
-          Velg utført aktivitet <span>({value.gy.value})</span>
-        </p>
-      </div>
-
-      <div className="input-list-container">
-        <ul className="input-activity-list">
-          {weeklyActivities}
-          {generalActivities}
-          {extras}
-        </ul>
-      </div>
-    </div>
-  );
+    );
 };
 
 export default TaskList;
