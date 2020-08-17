3c50916f74b34f88bcf8f851dcf10309f79c23da
Hans Gunleiksrud <hansag91@gmail.com>
2020-08-17 15:01:46 +0200

    started on circular activity list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,11 @@
 import React, {
-    Fragment,
-    useState,
-    useContext,
-    useMemo,
-    useEffect,
-    createContext,
-    useCallback,
+  Fragment,
+  useState,
+  useContext,
+  useMemo,
+  useEffect,
+  createContext,
+  useCallback,
 } from "react";
 import ReactDOM from "react-dom";
 import "./App.css";
@@ -14,6 +14,7 @@ import { GlobalEdit } from "./contexts/GlobalEdit";
 import { FieldButtons } from "./contexts/FieldButtons";
 import { ProjectContext } from "./contexts/ProjectContext";
 import { ProgressContext } from "./contexts/ProgressContext";
+import { ActivityViewContext } from "./contexts/ActivityViewContext";
 
 import Dropdown from "./components/Dropdown";
 import Map from "./components/Map";
@@ -27,68 +28,63 @@ import ProjectInfo from "./components/ProjectInfo";
 import { graveyards, weeklyActivities } from "./utilities/GraveyardInfo";
 
 function App() {
-    const [value, setValue] = useState({ field: "A", gy: graveyards[1] });
-    const providerValue = useMemo(() => ({ value, setValue }), [
-        value,
-        setValue,
-    ]);
-    const [edit, setEdit] = useState(false);
-    const [allClicked, setAllClicked] = useState(1);
-    const [viewProject, setViewProject] = useState(null);
-    const [temporaryProgress, setTemporaryProgress] = useState(null);
+  const [value, setValue] = useState({ field: "A", gy: graveyards[1] });
+  const providerValue = useMemo(() => ({ value, setValue }), [value, setValue]);
+  const [edit, setEdit] = useState(false);
+  const [allClicked, setAllClicked] = useState(1);
+  const [viewProject, setViewProject] = useState(null);
+  const [temporaryProgress, setTemporaryProgress] = useState(null);
+  const [selectedActivity, setSelectedActivity] = useState(null);
 
-    const Currentfields = value.gy.fields;
+  const Currentfields = value.gy.fields;
 
-    useEffect(() => {}, [edit]);
+  useEffect(() => {}, [edit]);
 
-    // bør kanskje legge til setMetode på value
-    if (!edit) {
-        return (
-            <GlobalContext.Provider value={providerValue}>
-                <ProjectContext.Provider
-                    value={{ viewProject, setViewProject }}
-                >
-                    <ProgressContext.Provider
-                        value={{ temporaryProgress, setTemporaryProgress }}
-                    >
-                        <div className="container">
-                            <div className="upper-bar">
-                                <Dropdown
-                                    title="Velg kirkegård"
-                                    items={graveyards}
-                                />
-                                <GlobalEdit.Provider value={{ edit, setEdit }}>
-                                    <RegisterWork />
-                                </GlobalEdit.Provider>
-                            </div>
+  // bør kanskje legge til setMetode på value
+  if (!edit) {
+    return (
+      <GlobalContext.Provider value={providerValue}>
+        <ProjectContext.Provider value={{ viewProject, setViewProject }}>
+          <ProgressContext.Provider
+            value={{ temporaryProgress, setTemporaryProgress }}
+          >
+            <ActivityViewContext.Provider
+              value={{ selectedActivity, setSelectedActivity }}
+            >
+              <div className="container">
+                <div className="upper-bar">
+                  <Dropdown title="Velg kirkegård" items={graveyards} />
+                  <GlobalEdit.Provider value={{ edit, setEdit }}>
+                    <RegisterWork />
+                  </GlobalEdit.Provider>
+                </div>
 
-                            <Map />
-                            <div className="gy-holder">
-                                <Fields fields={Currentfields} />
-                                <InfoBox />
-                            </div>
-                        </div>
-                        <div className="activity-box">
-                            <Activities />
-                        </div>
-                    </ProgressContext.Provider>
-                </ProjectContext.Provider>
-            </GlobalContext.Provider>
-        );
-    } else
-        return (
-            <GlobalContext.Provider value={providerValue}>
-                <GlobalEdit.Provider value={{ edit, setEdit }}>
-                    <div className="input-body">
-                        <FieldButtons.Provider
-                            value={{ allClicked, setAllClicked }}
-                        >
-                            <TaskList />
-                        </FieldButtons.Provider>
-                    </div>
-                </GlobalEdit.Provider>
-            </GlobalContext.Provider>
-        );
+                <Map />
+                <div className="gy-holder">
+                  <Fields fields={Currentfields} />
+                  <InfoBox />
+                </div>
+              </div>
+              <div className="activity-box">
+                <Activities />
+              </div>
+            </ActivityViewContext.Provider>
+          </ProgressContext.Provider>
+        </ProjectContext.Provider>
+      </GlobalContext.Provider>
+    );
+  } else
+    return (
+      <GlobalContext.Provider value={providerValue}>
+        <GlobalEdit.Provider value={{ edit, setEdit }}>
+          <div className="input-body">
+            <FieldButtons.Provider value={{ allClicked, setAllClicked }}>
+              <TaskList />
+            </FieldButtons.Provider>
+          </div>
+        </GlobalEdit.Provider>
+      </GlobalContext.Provider>
+    );
 }
 
 export default App;
--- client/src/components/Fields.js
+++ client/src/components/Fields.js
@@ -1,10 +1,16 @@
 import React, { useState, useContext, Fragment } from "react";
 
-import MenuFieldButton from "./MenuFieldButton"
+import MenuFieldButton from "./MenuFieldButton";
 import { GlobalContext } from "../contexts/GlobalContext";
+import { FieldActivities, fieldActivities } from "../utilities/GraveyardInfo";
+import { ActivityViewContext } from "../contexts/ActivityViewContext";
 
 const Fields = ({ fields }) => {
   const { value, setValue } = useContext(GlobalContext);
+  const { selectedActivity, setSelectedActivity } = useContext(
+    ActivityViewContext
+  );
+  const [showList, setShowList] = useState(false);
 
   const handleOnClick = (item) => {
     setValue((prev) => {
@@ -12,16 +18,41 @@ const Fields = ({ fields }) => {
     });
   };
 
+  const handleActivitySelectorClick = () => {
+    setShowList(!showList);
+  };
+
   const currentFields = fields.map((item, index) => (
     <li className="field-list-item">
-      <MenuFieldButton item= {item} index = {index}/>
-        
+      <MenuFieldButton item={item} index={index} />
     </li>
   ));
+
+  const activityList = fieldActivities.map((item, index) => {
+    if (index < 5) {
+      return (
+        <li className="activity-selector-li">
+          <img src={item.img} />
+          <h5>{item.value}</h5>
+        </li>
+      );
+    }
+  });
 
   return (
     <div className="fields-box">
       <ul className="fields-list">{currentFields}</ul>
+      <div className="activity-selector" onClick={handleActivitySelectorClick}>
+        <h5>Visning</h5>
+        <div className="activity-selector-icon"></div>
+      </div>
+      <div
+        className={`activity-selector-list-container ${
+          showList ? "show-list" : ""
+        }`}
+      >
+        <ul className="activity-selector-list">{activityList}</ul>
+      </div>
     </div>
   );
 };
