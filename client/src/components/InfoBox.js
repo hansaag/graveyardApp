d8c4098acdc4fcce85617914f89a7da4ca64d0f0
hans <hansag91@gmail.com>
2021-08-12 18:25:30 +0200

    some more general refactoring of fieldactivites in the infobox component

--- client/src/components/InfoBox.js
+++ client/src/components/InfoBox.js
@@ -46,8 +46,6 @@ const InfoBox = ({ item }) => {
     }
   }, [value]);
 
-
-
   const showGlobalActivities = globalActivities.map((activity, index) => {
     const timeElapsed = findTimeDiff(rightItems[index]);
     return (
@@ -72,11 +70,9 @@ const InfoBox = ({ item }) => {
     const timeElapsed = findTimeDiff(items[index]);
     return (
       <div className={`field-activity-${index}`} key={index}>
-        <img className="activity-icon" src={activity.img}></img>
+        <img src={activity.img}></img>
         <div
-          className={`infobox-status-text ${
-            activity.yellow < timeElapsed ? "show-yellow-text" : ""
-          }
+          className={`${activity.yellow < timeElapsed ? "show-yellow-text" : ""}
             ${activity.red < timeElapsed ? "show-red-text" : ""}`}
         >
           {timeElapsed}
@@ -111,19 +107,14 @@ const Styled_InfoboxWrapper = styled.div`
   margin: 0 2% 0 2%;
   padding: 0;
   font-weight: 700;
-  flex-shrink: 5;
   min-height: 27.5vh;
   max-height: 29vh;
-
-  color: black;
-  vertical-align: text-bottom;
 `;
 
 const Styled_FieldGrid = styled.div`
   width: 60vw;
   height: 100%;
   border-radius: 10px;
-  font-size: 0.7em;
   margin: 0;
   border: 1px solid #486a47;
   background-color: white;
@@ -131,9 +122,9 @@ const Styled_FieldGrid = styled.div`
   display: grid;
   grid-template:
     [row1-start] "header header" 10% [row1-end]
-    [row1-start] "gaaklipper blomster" 30% [row1-end]
-    [row2-start] "sitteklipper hekkesaks" 30% [row2-end]
-    [row3-start] "trimmer ljaa" 30% [row3-end]
+    [row2-start] "gaaklipper blomster" 30% [row2-end]
+    [row3-start] "sitteklipper hekkesaks" 30% [row3-end]
+    [row4-start] "trimmer ljaa" 30% [row4-end]
     / 50% 50%;
 
   & h2 {
@@ -141,7 +132,7 @@ const Styled_FieldGrid = styled.div`
     padding: 0 0 0 0;
     margin: 5px 0 0 0;
     text-align: center;
-    font-size: 1.3em;
+    font-size: 1em;
     letter-spacing: 0.7px;
     word-spacing: 5px;
   }
@@ -150,19 +141,19 @@ const Styled_FieldGrid = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: space-evenly;
-    font-size: 1.1em;
-    
+    font-size: 0.9em;
 
     & div {
       display: flex;
       flex-direction: column;
       justify-content: center;
-    };
+
+    }
 
     & img {
       max-height: 4.5vh;
       margin-top: 12%;
-    };
+    }
   }
   & .field-activity-1 {
     grid-area: gaaklipper;
